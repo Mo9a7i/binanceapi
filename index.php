aaa18019ce03dff9e071027c6524569f924257ec
mo9a7i <mohannad.otaibi@gmail.com>
2018-11-12 18:04:19 +0300

    another update

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
+# HTML Formatting
+echo '
+  <html>
+  <head>
+  </head>
+  <body>
+';
+
 #Get Markets from API in JSON Format
 $JSON_BINANCE_MARKETS=file_get_contents("https://www.binance.com/api/v1/ticker/24hr", false);
 #Convert JSON to PHP Array for foreach loop
@@ -14,5 +22,9 @@ foreach($ARRAY_BINANCE_MARKETS as $MARKET) {
         echo "Market: ".$TICKER." Last Price: ".$MARKET->lastPrice."\n\r";
     }
 }
-
+# HTML Formatting
+echo '
+  </body>
+  </html>
+';
 ?>
